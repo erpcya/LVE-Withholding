f6b54d45018e704d310ecd38297c2f98d0695944
Dixon Martinez <Dixon Martinez@localhost>
2013-02-14 11:06:22 -0530

    Change name in scripts and delete comment in scripts

--- scripts/postgresql/migration_script_2889008111198968559_postgresql.sql
+++ scripts/postgresql/migration_script_2889008111198968559_postgresql.sql
@@ -1,27 +1,8 @@
-﻿-- 23/01/2013 09:50:16 AM VET
--- LVE Retention
---Alter Table CUST_PersonType 
---Add Constraint PK_CUST_PersonType PRIMARY KEY (CUST_PersonType_ID);
-
--- 23/01/2013 09:50:16 AM VET
--- LVE Retention
---Alter Table CUST_ConceptRetention
---Add Constraint PK_CUST_ConceptRetention PRIMARY KEY (CUST_ConceptRetention_ID);
-
--- 23/01/2013 09:50:16 AM VET
--- LVE Retention
---Alter Table CUST_RetentionType 
---Add Constraint PK_CUST_RetentionType PRIMARY KEY (CUST_RetentionType_ID);
-
 -- 23/01/2013 09:50:16 AM VET
 -- LVE Retention
 Alter Table CUST_RetentionType 
 Add Constraint FK_CUST_RetentionType_AD_Rule FOREIGN KEY (AD_Rule_ID)
 REFERENCES AD_Rule (AD_Rule_ID) On Update Cascade On Delete Restrict;
-
--- 23/01/2013 09:50:16 AM VET
--- LVE Retention
-Alter Table C_DocType Add Column CUST_RetentionType_ID NUMERIC(10) Default NULL;
 
 -- 23/01/2013 09:50:16 AM VET
 -- LVE Retention
@@ -38,21 +19,11 @@ On Update Cascade On Delete Restrict;
 
 -- 23/01/2013 09:50:16 AM VET
 -- LVE Retention
---Alter Table CUST_RetentionConfig 
---Add Constraint PK_CUST_RetentionConfig PRIMARY KEY (CUST_RetentionConfig_ID);
-
--- 23/01/2013 09:50:16 AM VET
--- LVE Retention
 Alter Table CUST_RetentionConfig 
 Add CONSTRAINT FK_CUST_RetentionConfig_CUST_RetentionType
 FOREIGN KEY (CUST_RetentionType_ID)
 REFERENCES CUST_RetentionType (CUST_RetentionType_ID)
 On Update Cascade On Delete Restrict;
-
--- 23/01/2013 09:50:16 AM VET
--- LVE Retention
---Alter Table CUST_RetentionRelation Add 
---Constraint PK_CUST_RetentionRelation PRIMARY KEY (CUST_RetentionRelation_ID);
 
 -- 23/01/2013 09:50:16 AM VET
 -- LVE Retention
@@ -82,11 +53,6 @@ Alter Table CUST_RetentionRelation Add
 CONSTRAINT FK_CUST_RetentionRelation_C_DocType FOREIGN KEY (C_DocType_ID)
 REFERENCES C_DocType (C_DocType_ID)
 On Update Cascade On Delete Restrict;
-
--- 23/01/2013 09:50:16 AM VET
--- LVE Retention
---Alter Table CUST_CR_PT_Combination Add 
---Constraint PK_CUST_CR_PT_Combination PRIMARY KEY (CUST_CR_PT_Combination_ID);
 
 -- 23/01/2013 09:50:16 AM VET
 -- LVE Retention

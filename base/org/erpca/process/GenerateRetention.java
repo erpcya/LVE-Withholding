7bbab9a86a8a4c5fcd8dccd7204d8150bb5a2b20
Yamel Senih <yamelsenih@gmail.com>
2013-02-26 15:55:15 -0530

    Changes in Generate Retention

--- base/org/erpca/process/GenerateRetention.java
+++ base/org/erpca/process/GenerateRetention.java
@@ -36,26 +36,23 @@ public class GenerateRetention extends SvrProcess {
 	private int			p_C_Invoice_ID			=	0;
 	/**	Business Partner				*/
 	private int			p_C_BPartner_ID			=	0;
+	/**	Retention Type					*/
+	private int			p_CUST_RetentionType_ID	=	0;
 	/**	Accounting	Date				*/
 	private Timestamp	p_DateAcct				=	null;
 	/**	Accounting	Date				*/
 	private Timestamp	p_DateAcct_To			=	null;
-	/**	Retention Type					*/
-	private String		p_CUST_RetentionType_ID	=	null;
-	/**	Record ID						*/
-	private int			record_ID				=	0;
 	
-	private String		sql 					= new String();
+	private String				sql 				= new String();
 	/**	Where Clause					*/
 	private StringBuffer		m_parameterWhere	= new StringBuffer();
-	
-	/**
-	 * 
-	 * *** Constructor de la Clase ***
-	 * @author Yamel Senih 19/02/2013, 12:14:38
+
+	/* (non-Javadoc)
+	 * @see org.compiere.process.SvrProcess#prepare()
 	 */
-	public GenerateRetention() {
-		//record_ID = getRecord_ID();
+	@Override
+	protected void prepare() {
+		
 		for (ProcessInfoParameter para:getParameter()){
 			String name = para.getParameterName();
 			if (para.getParameter() == null)
@@ -69,19 +66,26 @@ public class GenerateRetention extends SvrProcess {
 				p_DateAcct_To = (Timestamp)para.getParameter_To();
 			}
 			else if (name.equals("CUST_RetentionType_ID"))
-				p_CUST_RetentionType_ID = (String)para.getParameter();
-			record_ID = p_C_Invoice_ID != 0? p_C_Invoice_ID: getRecord_ID();
+				p_CUST_RetentionType_ID = para.getParameterAsInt();
 		}
-	}
-
-	/* (non-Javadoc)
-	 * @see org.compiere.process.SvrProcess#prepare()
-	 */
-	@Override
-	protected void prepare() {
-		sql = new String("SELECT bp.C_BPartner_ID, inv.C_Invoice_ID, rr.CUST_RetentionType_ID, " +
+		//	Set RecordID
+		p_C_Invoice_ID = getRecord_ID() != 0 && p_C_Invoice_ID == 0? getRecord_ID(): p_C_Invoice_ID;
+		
+		if(p_C_Invoice_ID != 0)
+			m_parameterWhere.append("AND inv.C_Invoice_ID = " + p_C_Invoice_ID + " ");
+		if(p_C_BPartner_ID != 0)
+			m_parameterWhere.append("AND inv.C_BPartner_ID = " + p_C_BPartner_ID + " ");
+		if(p_DateAcct != null)
+			m_parameterWhere.append("AND inv.DateAcct >= " + DB.TO_DATE(p_DateAcct) + " ");
+		if(p_DateAcct_To != null)
+			m_parameterWhere.append("AND inv.DateAcct <= " + DB.TO_DATE(p_DateAcct_To) + " ");
+		if(p_CUST_RetentionType_ID != 0)
+			m_parameterWhere.append("AND rr.CUST_RetentionType_ID = " + p_CUST_RetentionType_ID + " ");
+		
+		//	
+		sql = new String("SELECT bp.C_BPartner_ID, inv.C_Invoice_ID, rr.CUST_RetentionType_ID, rt.C_Charge_ID, rt.C_DocType_ID, " +
 				"SUM(CASE WHEN linv.IsRetaint = 'Y' THEN linv.LineNetAmt ELSE 0 END) LineNetAmt, " +
-				"cn.Aliquot, cn.MinimalAmt, cn.Subtrahend, rt.C_Charge_ID, rt.C_DocType_ID " + 
+				"cn.Aliquot, cn.MinimalAmt, cn.Subtrahend " + 
 				"FROM C_Invoice inv " + 
 				"INNER JOIN C_InvoiceLine linv ON(linv.C_Invoice_ID = inv.C_Invoice_ID) " + 
 				"INNER JOIN C_BPartner bp ON(bp.C_BPartner_ID = inv.C_BPartner_ID) " + 
@@ -95,8 +99,11 @@ public class GenerateRetention extends SvrProcess {
 				"	(inv.CUST_ConceptRetention_ID = cb.CUST_ConceptRetention_ID AND bp.CUST_PersonType_ID = cb.CUST_PersonType_ID) " + 
 				"	OR(cb.CUST_ConceptRetention_ID IS NULL AND cb.CUST_PersonType_ID IS NULL) " + 
 				"	) " + 
-				"GROUP BY bp.C_BPartner_ID, inv.C_Invoice_ID, rr.CUST_RetentionType_ID, " +
-				"cn.Aliquot, cn.MinimalAmt, cn.Subtrahend, rt.C_Charge_ID, rt.C_DocType_ID " + 
+				//	Sql Where
+				m_parameterWhere.toString() + 
+				//	Group By
+				"GROUP BY bp.C_BPartner_ID, inv.C_Invoice_ID, rr.CUST_RetentionType_ID, rt.C_Charge_ID, rt.C_DocType_ID, " +
+				"cn.Aliquot, cn.MinimalAmt, cn.Subtrahend " + 
 				"ORDER BY bp.C_BPartner_ID, inv.C_Invoice_ID, rr.CUST_RetentionType_ID, rt.C_Charge_ID, rt.C_DocType_ID");
 		
 	}
@@ -105,21 +112,30 @@ public class GenerateRetention extends SvrProcess {
 	protected String doIt() throws Exception {
 		PreparedStatement pstmt = null;
 		pstmt = DB.prepareStatement(sql, null);
-		ResultSet res = pstmt.executeQuery();
+		ResultSet rs = pstmt.executeQuery();
 		
-		int m_C_BPartner_ID = 0;
 		int m_C_Invoice_ID = 0;
+		int m_C_BPartner_ID = 0;
 		int m_CUST_RetentionType_ID = 0;
+		int m_C_Charge_ID = 0;
+		int m_C_DocType_ID = 0;
 		BigDecimal m_LineNetAmt = Env.ZERO;
 		BigDecimal m_Aliquot = Env.ZERO;
 		BigDecimal m_MinimalAmt = Env.ZERO;
 		BigDecimal m_Subtrahend = Env.ZERO;
-		int m_C_Charge_ID = 0;
-		int m_C_DocType_ID = 0;
 		
 		
-		if(res != null){
-			while(res.next()){
+		if(rs != null){
+			while(rs.next()){
+				m_C_Invoice_ID 				= rs.getInt("C_Invoice_ID");
+				m_C_BPartner_ID 			= rs.getInt("C_BPartner_ID");
+				m_CUST_RetentionType_ID 	= rs.getInt("CUST_RetentionType_ID");
+				m_C_Charge_ID 				= rs.getInt("C_Charge_ID");
+				m_C_DocType_ID 				= rs.getInt("C_DocType_ID");
+				m_LineNetAmt 				= rs.getBigDecimal("LineNetAmt");
+				m_Aliquot 					= rs.getBigDecimal("Aliquot");
+				m_MinimalAmt 				= rs.getBigDecimal("MinimalAmt");
+				m_Subtrahend 				= rs.getBigDecimal("Subtrahend");
 				
 			}
 		}

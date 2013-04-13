84e8cdba64ac94eaac4d9036f225d16534982164
Yamel Senih <Yamel Senih@localhost>
2013-04-13 15:05:54 -0530

    Changes in INALSA

---
+++ migration/postgresql/028Fix_Process_Retention_postgresql.sql
@@ -0,0 +1,95 @@
+-- Apr 13, 2013 3:15:15 PM VET
+-- LVE Retention
+UPDATE AD_Process_Para SET SeqNo=0,Updated=TO_TIMESTAMP('2013-04-13 15:15:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=3000001
+;
+
+-- Apr 13, 2013 3:15:21 PM VET
+-- LVE Retention
+UPDATE AD_Process_Para SET SeqNo=20,Updated=TO_TIMESTAMP('2013-04-13 15:15:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=3000000
+;
+
+-- Apr 13, 2013 3:15:25 PM VET
+-- LVE Retention
+UPDATE AD_Process_Para SET SeqNo=10,Updated=TO_TIMESTAMP('2013-04-13 15:15:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=3000001
+;
+
+-- Apr 13, 2013 3:16:14 PM VET
+-- LVE Retention
+UPDATE AD_Process_Para SET AD_Val_Rule_ID=145,Updated=TO_TIMESTAMP('2013-04-13 15:16:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=3000000
+;
+
+-- Apr 13, 2013 3:23:03 PM VET
+-- LVE Retention
+UPDATE AD_Field SET SeqNo=60,IsDisplayed='Y' WHERE AD_Field_ID=3000014
+;
+
+-- Apr 13, 2013 3:23:03 PM VET
+-- LVE Retention
+UPDATE AD_Field SET SeqNo=70,IsDisplayed='Y' WHERE AD_Field_ID=3000010
+;
+
+-- Apr 13, 2013 3:23:03 PM VET
+-- LVE Retention
+UPDATE AD_Field SET SeqNo=80,IsDisplayed='Y' WHERE AD_Field_ID=3000009
+;
+
+-- Apr 13, 2013 3:28:34 PM VET
+-- LVE Retention
+UPDATE AD_Field SET IsSameLine='Y',Updated=TO_TIMESTAMP('2013-04-13 15:28:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3000014
+;
+
+-- Apr 13, 2013 3:28:42 PM VET
+-- LVE Retention
+UPDATE AD_Field SET IsSameLine='Y',Updated=TO_TIMESTAMP('2013-04-13 15:28:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3000009
+;
+
+-- Apr 13, 2013 3:29:13 PM VET
+-- LVE Retention
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=3000009
+;
+
+-- Apr 13, 2013 3:29:14 PM VET
+-- LVE Retention
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=3000010
+;
+
+-- Apr 13, 2013 3:29:51 PM VET
+-- LVE Retention
+UPDATE AD_Column SET DefaultValue='NULL',Updated=TO_TIMESTAMP('2013-04-13 15:29:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3000065
+;
+
+-- Apr 13, 2013 3:30:00 PM VET
+-- LVE Retention
+UPDATE AD_Column SET DefaultValue='NULL',Updated=TO_TIMESTAMP('2013-04-13 15:30:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3000064
+;
+
+-- Apr 13, 2013 3:32:37 PM VET
+-- LVE Retention
+UPDATE AD_Field SET SeqNo=70,IsDisplayed='Y' WHERE AD_Field_ID=3000009
+;
+
+-- Apr 13, 2013 3:32:37 PM VET
+-- LVE Retention
+UPDATE AD_Field SET SeqNo=80,IsDisplayed='Y' WHERE AD_Field_ID=3000010
+;
+
+-- Apr 13, 2013 3:33:03 PM VET
+-- LVE Retention
+UPDATE AD_Field SET SeqNo=70,IsDisplayed='Y' WHERE AD_Field_ID=3000010
+;
+
+-- Apr 13, 2013 3:33:03 PM VET
+-- LVE Retention
+UPDATE AD_Field SET SeqNo=80,IsDisplayed='Y' WHERE AD_Field_ID=3000009
+;
+
+-- Apr 13, 2013 3:33:55 PM VET
+-- LVE Retention
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2013-04-13 15:33:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3000010
+;
+
+-- Apr 13, 2013 3:33:58 PM VET
+-- LVE Retention
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2013-04-13 15:33:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3000009
+;
+

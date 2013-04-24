b776280b785ab9653da7aa8d461819c500476e63
Dixon Martinez <Dixon Martinez@localhost>
2013-04-24 14:46:06 -0530

    Update Scripts

--- sql/Compañia/002_Update_Column_Translate_ReportView.sql
+++ sql/Compañia/002_Update_Column_Translate_ReportView.sql
@@ -1,4 +1,4 @@
-UPDATE AD_PrintFormatItem_Trl SET AD_Client_ID = 1000002, AD_Org_ID = 1000002
+UPDATE AD_PrintFormatItem_Trl SET AD_Client_ID = 1000002, AD_Org_ID = 0
 WHERE AD_PrintFormatItem_ID IN (
 SELECT AD_PrintFormatItem_ID FROM AD_PrintFormatItem 
 WHERE AD_PrintFormat_ID IN (SELECT AD_PrintFormat_ID FROM AD_PrintFormat

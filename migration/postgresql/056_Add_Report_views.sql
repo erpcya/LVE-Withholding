c12dbd5a2fbc0f66a882152154bfd5e9de4cdf1a
Dixon Martinez <Dixon Martinez@localhost>
2013-04-29 16:40:18 -0530

    Add Report Books Purchases and Sales

---
+++ migration/postgresql/056_Add_Report_views.sql
@@ -0,0 +1,11 @@
+-- 29/04/2013 11:28:49 AM VET
+-- LVE Retention
+INSERT INTO AD_ReportView (AD_Client_ID,AD_Org_ID,AD_ReportView_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,WhereClause) VALUES (0,0,3000203,3000217,TO_TIMESTAMP('2013-04-29 11:28:49','YYYY-MM-DD HH24:MI:SS'),100,'ECA02','Y','Books Purchases',TO_TIMESTAMP('2013-04-29 11:28:49','YYYY-MM-DD HH24:MI:SS'),100,'IsSOTrx = ''N''')
+;
+
+
+-- 29/04/2013 11:31:01 AM VET
+-- LVE Retention
+INSERT INTO AD_ReportView (AD_Client_ID,AD_Org_ID,AD_ReportView_ID,AD_Table_ID,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,WhereClause) VALUES (0,0,3000204,3000217,TO_TIMESTAMP('2013-04-29 11:31:00','YYYY-MM-DD HH24:MI:SS'),100,'U','Y','Books Sales',TO_TIMESTAMP('2013-04-29 11:31:00','YYYY-MM-DD HH24:MI:SS'),100,'IsSOTrx = ''Y''')
+;
+

64d28f9048c8a6783aadfcc3c43b6e5975ffbd3c
Dixon Martinez <Dixon Martinez@localhost>
2013-04-30 16:53:37 -0530

    Add Scripts Books Sales and Purchases

---
+++ migration/postgresql/060_Change_Reference.sql
@@ -0,0 +1,14 @@
+-- Apr 30, 2013 9:46:35 AM VET
+-- LVE Retention
+UPDATE AD_Column SET AD_Reference_ID=18, AD_Reference_Value_ID=53275,Updated=TO_TIMESTAMP('2013-04-30 09:46:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3000439
+;
+
+-- 30/04/2013 03:10:09 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET AD_Reference_ID=15,Updated=TO_TIMESTAMP('2013-04-30 15:10:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3000428
+;
+
+-- 30/04/2013 03:10:19 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Column SET AD_Reference_ID=15,Updated=TO_TIMESTAMP('2013-04-30 15:10:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=3000427
+;

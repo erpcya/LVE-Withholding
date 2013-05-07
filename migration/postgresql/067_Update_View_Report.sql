914c6300a5adb439d7a05753887c2c3fd4240a0d
Dixon Martinez <dixon22ma@users.noreply.github.com>
2013-05-07 08:33:54 -0530

    update view report

---
+++ migration/postgresql/067_Update_View_Report.sql
@@ -0,0 +1,10 @@
+-- 3/05/2013 05:51:36 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_ReportView SET WhereClause='CUST_RV_Books.IsSOTrx = ''N''',Updated=TO_TIMESTAMP('2013-05-03 17:51:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_ReportView_ID=3000203
+;
+
+-- 3/05/2013 05:51:44 PM VET
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_ReportView SET EntityType='ECA02', WhereClause='CUST_RV_Books.IsSOTrx = ''Y''',Updated=TO_TIMESTAMP('2013-05-03 17:51:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_ReportView_ID=3000204
+;
+

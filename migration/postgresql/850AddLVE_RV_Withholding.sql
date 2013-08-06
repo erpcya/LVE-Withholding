0330d45781bcdb6d07ff697bc5204b4134495339
Dixon Martinez <Dixon Martinez@localhost>
2013-08-06 07:58:38 -0530

    Updae view LVE_RV_Withholding and add format print Withholding IVA

--- migration/postgresql/850AddLVE_RV_Withholding.sql
+++ migration/postgresql/850AddLVE_RV_Withholding.sql
@@ -1,84 +1,51 @@
+--DROP VIEW LVE_RV_Withholding
 CREATE OR REPLACE VIEW LVE_RV_Withholding AS 
-SELECT 	DISTINCT
-	-- Invoice	
-	i.AD_Client_ID, --ID Client
-	i.AD_Org_ID, --ID Org
-	i.DateInvoiced, --Date Invoiced
-	i.ControlNo,--Control Number
-	i.TotalLines, -- Amount Lines Not Tax of Invoice
-	i.GrandTotal, -- Total Amount 
-	-- Invoice Line
-	il.C_Invoice_ID, -- Invoice ID from Invoice Line
-	il.DocAffected_ID, -- Invoiced Affected
-	il.LineNetAmt, -- Line Amount Net
-	il.Line / 10::NUMERIC AS Line, -- Number Line Invoice Line
-	--Document Infor
-	dt.Name, -- Document Name 
-	-- Tax Info
-	tax.Rate, -- Tax Rate
-	tax.taxamt, -- Tax Amount
-	tax.taxbaseamt, -- Tax Base Amount
-
-	-- Invoice Affected
-	iaffected.DocumentNo, --Document No Affected
-	iaffected.Amount, -- Amount Invoice Affected
-
-	-- WH_Conbination 
-	whc.Aliquot, --Aliquot
-	whc.value, -- Search Key Withholding Concept
-
-	-- Withholding Config
-	wc.Subtrahend,-- Subtrahend
-
-	-- Withholding Type
-	wt.value AS value_retentiongroup,  --Value Withholding Type
-	wt.name AS name_retentiongroup --Name Withholding Type
-	
-FROM C_DocType dt
-JOIN C_Invoice i ON i.C_DocType_ID = dt.C_DocType_ID
-JOIN C_InvoiceLine il ON i.C_Invoice_ID = il.DocAffected_ID
-JOIN LVE_WithholdingConfig wc ON wc.LVE_WithholdingConfig_ID = il.LVE_WithholdingConfig_ID
-JOIN LVE_WH_Combination whc ON whc.LVE_WH_Combination_ID = wc.LVE_WH_Combination_ID
-JOIN LVE_WithholdingConcept wct ON wct.LVE_WithholdingConcept_ID = whc.LVE_WithholdingConcept_ID
-JOIN LVE_Withholding w ON w.LVE_Withholding_ID = wct.LVE_Withholding_ID
+SELECT DISTINCT
+	ci.ad_client_id, 
+	ci.ad_org_id, 
+	cil.c_invoice_id, 
+	ci.c_invoice_id AS docaffected_id, 
+	ci.dateinvoiced, 
+	ci.controlno, 
+	cdt.name, 
+	ctax.rate, 
+	ctax.taxamt, 
+	ctax.taxbaseamt, 
+	ci.totallines, 
+	ci.grandtotal, 
+	cil.linenetamt, 
+	ciaffected.documentno,
+	ciaffected.amount, 
+	whc.aliquot, 
+	wcc.subtrahend, 
+	wt.value AS withholdinggroupvalue, 
+	wt.name AS name_retentiongroup, 
+	cil.line / 10::numeric AS line,
+	whc.value
+FROM c_doctype cdt
+JOIN c_invoice ci ON ci.c_doctype_id = cdt.c_doctype_id
+JOIN c_invoiceline cil ON ci.c_invoice_id = cil.docaffected_id
+JOIN LVE_WithholdingConcept wc ON wc.LVE_WithholdingConcept_ID = ci.LVE_WithholdingConcept_ID
+JOIN LVE_WH_Combination whc ON whc.LVE_WithholdingConcept_ID = wc.LVE_WithholdingConcept_ID
+JOIN LVE_WithholdingConfig wcc ON wcc.LVE_WH_Combination_ID = whc.LVE_WH_Combination_ID
+JOIN LVE_Withholding w ON wc.LVE_Withholding_ID = w.LVE_Withholding_ID
 JOIN LVE_WithholdingType wt ON wt.LVE_WithholdingType_ID = w.LVE_WithholdingType_ID
-JOIN 
-(
-	SELECT
-		it.C_Invoice_ID, --Invoice of Tax
-		MAX(t.Rate) AS Rate, -- ID Max Tax
-		SUM(it.TaxAmt) AS TaxAmt, -- Tax Amount
-		SUM(
-			CASE t.Rate 
-				WHEN 0 THEN 0:: NUMERIC
-				ELSE it.TaxBaseAmt
-			END
-		) AS TaxBaseAmt -- Amount base Tax
-	FROM  C_InvoiceTax it 
-	JOIN C_Tax t ON it.C_Tax_ID = t.C_Tax_ID 
-	GROUP BY
-		it.C_Invoice_ID
-)tax ON tax.C_Invoice_ID = i.C_Invoice_ID
-LEFT JOIN 
-(
-	SELECT 
-		al.C_Invoice_ID AS Withholding_ID,
-		al.C_Invoice_ID,
-		i.DocumentNo, 
-		calrel.Amount
-	FROM C_AllocationHdr a 
-	JOIN C_AllocationLine al ON a.C_AllocationHdr_ID = al.C_AllocationHdr_ID
-	JOIN c_allocationline calrel ON a.c_allocationhdr_id = calrel.c_allocationhdr_id
-	JOIN C_Invoice i ON i.C_Invoice_ID = calrel.C_Invoice_ID
-	WHERE NOT 
-		(
-			EXISTS (
-				SELECT 1
-				FROM LVE_Withholding w
-				WHERE 
-					w.WithholdingDocType = i.C_DocType_ID :: CHARACTER
-				)
-		)
-	
-	
-)iaffected ON iaffected.Withholding_ID = i.c_invoice_id AND iaffected.c_invoice_id <> i.c_invoice_id
+JOIN ( SELECT citax.c_invoice_id, max(ctax.rate) AS rate, 
+    sum(citax.taxamt) AS taxamt, 
+    sum(
+        CASE ctax.rate
+            WHEN 0 THEN 0::numeric
+            ELSE citax.taxbaseamt
+        END) AS taxbaseamt
+   FROM c_invoicetax citax
+   JOIN c_tax ctax ON citax.c_tax_id = ctax.c_tax_id
+  GROUP BY citax.c_invoice_id) ctax ON ctax.c_invoice_id = ci.c_invoice_id
+   LEFT JOIN ( SELECT cal.c_invoice_id AS retention_id, calrel.c_invoice_id, 
+    ci.documentno, calrel.amount
+   FROM c_allocationhdr cah
+   JOIN c_allocationline cal ON cah.c_allocationhdr_id = cal.c_allocationhdr_id
+   JOIN c_allocationline calrel ON cah.c_allocationhdr_id = calrel.c_allocationhdr_id
+   JOIN c_invoice ci ON ci.c_invoice_id = calrel.c_invoice_id
+  WHERE NOT (EXISTS ( SELECT 1
+   FROM LVE_Withholding
+  WHERE LVE_Withholding.WithholdingDocType = ci.c_doctype_id::bpchar))) ciaffected ON ciaffected.retention_id = ci.c_invoice_id AND ciaffected.c_invoice_id <> ci.c_invoice_id;

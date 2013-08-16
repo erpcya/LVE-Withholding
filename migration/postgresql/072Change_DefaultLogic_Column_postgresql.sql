aed4ad0dc1721002847adced2cd1c6dc2588848c
Carlos Parada <carlosaparadam@gmail.com>
2013-08-15 19:34:53 -0530

    Changes in WH_Config And WH_Combination

---
+++ migration/postgresql/072Change_DefaultLogic_Column_postgresql.sql
@@ -0,0 +1,5 @@
+-- Aug 10, 2013 1:54:05 PM VET
+-- LVE Withholding
+UPDATE AD_Column SET DefaultValue=NULL,Updated=TO_TIMESTAMP('2013-08-10 13:54:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=66488
+;
+

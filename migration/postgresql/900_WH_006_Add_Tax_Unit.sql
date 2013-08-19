4418d545bb76b2dce23a486177ff0c9717b6e642
carlosaparadam <carlosaparadam@localhost>
2013-08-19 00:50:45 -0530

    Add Views And Add Data to GardenWorld

---
+++ migration/postgresql/900_WH_006_Add_Tax_Unit.sql
@@ -0,0 +1,5 @@
+-- Aug 16, 2013 4:03:20 PM VET
+-- LVE Retention
+INSERT INTO LVE_TaxUnit (AD_Client_ID,AD_Org_ID,Created,CreatedBy,Description,IsActive,LVE_TaxUnit_ID,Name,Processing,TaxAmt,Updated,UpdatedBy,ValidFrom) VALUES (11,0,TO_TIMESTAMP('2013-08-16 16:03:20','YYYY-MM-DD HH24:MI:SS'),100,'Unidad Tributaria 2013','Y',1000001,'U.T. 2013','N',107.000000000000,TO_TIMESTAMP('2013-08-16 16:03:20','YYYY-MM-DD HH24:MI:SS'),100,TO_TIMESTAMP('2013-02-06','YYYY-MM-DD'))
+;
+

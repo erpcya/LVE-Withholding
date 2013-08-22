d00223535f2eb7f4b8eab1624b2de812c987a4e4
Dixon Martinez <dixon22ma@users.noreply.github.com>
2013-08-22 15:26:25 -0530

    Update Script tables into SGDB, add element, column from nationality and authorized officer, view LVE_RV_WH_WH_List into ADempiere, add reference combination name and report withholding list.

---
+++ migration/oracle/101UpdateColumn_NationalityAndAuthorizedOfficer.sql
@@ -0,0 +1,15 @@
+-- 22/08/2013 11:29:31 AM VET
+-- LVE Withholding
+UPDATE AD_Column SET IsUpdateable='Y',Updated=TO_DATE('2013-08-22 11:29:31','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=66811
+;
+
+-- 22/08/2013 11:30:38 AM VET
+-- LVE Withholding
+UPDATE AD_Column SET AD_Val_Rule_ID=230,Updated=TO_DATE('2013-08-22 11:30:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=66812
+;
+
+-- 22/08/2013 11:35:19 AM VET
+-- LVE Withholding
+UPDATE AD_Column SET AD_Reference_Value_ID=138,Updated=TO_DATE('2013-08-22 11:35:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=66812
+;
+

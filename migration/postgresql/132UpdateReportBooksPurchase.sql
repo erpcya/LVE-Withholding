a67259fc64dc8813387e782ab799bb67bbdde9ed
Dixon Martinez <Dixon Martinez@localhost>
2013-09-23 15:42:46 -0530

    Add process Books Purchases, translation report

---
+++ migration/postgresql/132UpdateReportBooksPurchase.sql
@@ -0,0 +1,95 @@
+-- 22/09/2013 09:47:37 PM VET
+-- LVE Withholding
+UPDATE AD_PrintFormatItem SET IsCentrallyMaintained='N', IsGroupBy='N', IsPageBreak='N', SortNo=0, XPosition=0, YPosition=0,Updated=TO_TIMESTAMP('2013-09-22 21:47:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=54351
+;
+
+-- 22/09/2013 09:47:44 PM VET
+-- LVE Withholding
+UPDATE AD_PrintFormatItem_Trl SET PrintName='No. Factura',Updated=TO_TIMESTAMP('2013-09-22 21:47:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=54351 AND AD_Language='es_MX'
+;
+
+-- 22/09/2013 09:47:52 PM VET
+-- LVE Withholding
+UPDATE AD_PrintFormatItem SET IsCentrallyMaintained='N', IsGroupBy='N', IsPageBreak='N', SortNo=0, XPosition=0, YPosition=0,Updated=TO_TIMESTAMP('2013-09-22 21:47:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=54356
+;
+
+-- 22/09/2013 09:48:02 PM VET
+-- LVE Withholding
+UPDATE AD_PrintFormatItem_Trl SET PrintName='No. Control',Updated=TO_TIMESTAMP('2013-09-22 21:48:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=54356 AND AD_Language='es_MX'
+;
+
+-- 22/09/2013 09:57:02 PM VET
+-- LVE Withholding
+UPDATE AD_Client SET AD_Language='es_MX', IsMultiLingualDocument='Y',Updated=TO_TIMESTAMP('2013-09-22 21:57:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Client_ID=11
+;
+
+-- 22/09/2013 09:57:54 PM VET
+-- LVE Withholding
+UPDATE AD_PrintFormatItem SET IsCentrallyMaintained='N', IsGroupBy='N', IsPageBreak='N', SortNo=0, XPosition=0, YPosition=0,Updated=TO_TIMESTAMP('2013-09-22 21:57:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=54347
+;
+
+-- 22/09/2013 09:58:03 PM VET
+-- LVE Withholding
+UPDATE AD_PrintFormatItem_Trl SET PrintName='Nota de Crédito',Updated=TO_TIMESTAMP('2013-09-22 21:58:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=54347 AND AD_Language='es_MX'
+;
+
+-- 22/09/2013 09:58:15 PM VET
+-- LVE Withholding
+UPDATE AD_PrintFormatItem_Trl SET PrintName='Nota de Débito',Updated=TO_TIMESTAMP('2013-09-22 21:58:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=54348 AND AD_Language='es_MX'
+;
+
+-- 22/09/2013 09:58:20 PM VET
+-- LVE Withholding
+UPDATE AD_PrintFormatItem SET IsCentrallyMaintained='N', IsGroupBy='N', IsPageBreak='N', SortNo=0, XPosition=0, YPosition=0,Updated=TO_TIMESTAMP('2013-09-22 21:58:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=54350
+;
+
+-- 22/09/2013 09:58:28 PM VET
+-- LVE Withholding
+UPDATE AD_PrintFormatItem_Trl SET PrintName='Factura Afectada',Updated=TO_TIMESTAMP('2013-09-22 21:58:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=54350 AND AD_Language='es_MX'
+;
+
+-- 22/09/2013 09:59:57 PM VET
+-- LVE Withholding
+UPDATE AD_PrintFormat SET Name='Libro de Compras',Updated=TO_TIMESTAMP('2013-09-22 21:59:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormat_ID=50128
+;
+
+-- 22/09/2013 10:00:49 PM VET
+-- LVE Withholding
+UPDATE AD_Process_Trl SET Name='Libro de Compras',Updated=TO_TIMESTAMP('2013-09-22 22:00:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=53457 AND AD_Language='es_MX'
+;
+
+-- 22/09/2013 10:01:02 PM VET
+-- LVE Withholding
+UPDATE AD_Menu SET EntityType='ECA02',Updated=TO_TIMESTAMP('2013-09-22 22:01:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=53619
+;
+
+-- 22/09/2013 10:01:10 PM VET
+-- LVE Withholding
+UPDATE AD_Menu_Trl SET Name='Libro de Compras',Updated=TO_TIMESTAMP('2013-09-22 22:01:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=53619 AND AD_Language='es_MX'
+;
+
+-- 22/09/2013 10:02:17 PM VET
+-- LVE Withholding
+UPDATE AD_PrintFormatItem SET IsCentrallyMaintained='N', IsGroupBy='N', IsPageBreak='N', SortNo=0, XPosition=0, YPosition=0,Updated=TO_TIMESTAMP('2013-09-22 22:02:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=54363
+;
+
+-- 22/09/2013 10:02:23 PM VET
+-- LVE Withholding
+UPDATE AD_PrintFormatItem_Trl SET PrintName='Tipo de Doc.',Updated=TO_TIMESTAMP('2013-09-22 22:02:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=54363 AND AD_Language='es_MX'
+;
+
+-- 22/09/2013 10:02:49 PM VET
+-- LVE Withholding
+UPDATE AD_PrintFormatItem SET IsGroupBy='N', IsPageBreak='N', MaxWidth=70, SortNo=0, XPosition=0, YPosition=0,Updated=TO_TIMESTAMP('2013-09-22 22:02:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=54357
+;
+
+-- 22/09/2013 10:07:11 PM VET
+-- LVE Withholding
+UPDATE AD_PrintFormatItem SET IsGroupBy='N', IsPageBreak='N', MaxWidth=65, SortNo=0, XPosition=0, YPosition=0,Updated=TO_TIMESTAMP('2013-09-22 22:07:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=54356
+;
+
+-- 22/09/2013 10:07:46 PM VET
+-- LVE Withholding
+UPDATE AD_PrintFormatItem_Trl SET PrintName='Proveedor.',Updated=TO_TIMESTAMP('2013-09-22 22:07:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_PrintFormatItem_ID=54345 AND AD_Language='es_MX'
+;
+

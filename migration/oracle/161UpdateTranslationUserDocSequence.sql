3dd5f4aa67d994d395fa9ae315672abf0fdc80c5
Dixon Martinez <dixon22ma@users.noreply.github.com>
2014-02-21 10:44:55 -0530

    Update Translation User Doc Sequence

---
+++ migration/oracle/161UpdateTranslationUserDocSequence.sql
@@ -0,0 +1,30 @@
+-- Feb 21, 2014 11:42:02 AM VET
+-- LVE Withholding
+UPDATE AD_Table_Trl SET Name='Secuencia de Documento por Usuario',Updated=TO_DATE('2014-02-21 11:42:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=53751 AND AD_Language='es_VE'
+;
+
+-- Feb 21, 2014 11:42:12 AM VET
+-- LVE Withholding
+UPDATE AD_Table_Trl SET Name='Secuencia de Documento por Usuario',Updated=TO_DATE('2014-02-21 11:42:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=53751 AND AD_Language='es_MX'
+;
+
+-- Feb 21, 2014 11:42:25 AM VET
+-- LVE Withholding
+UPDATE AD_Tab_Trl SET Name='Secuencia de Documento por Usuario',Updated=TO_DATE('2014-02-21 11:42:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53893 AND AD_Language='es_MX'
+;
+
+-- Feb 21, 2014 11:42:29 AM VET
+-- LVE Withholding
+UPDATE AD_Tab_Trl SET Name='Secuencia de Documento por Usuario',Updated=TO_DATE('2014-02-21 11:42:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53893 AND AD_Language='es_VE'
+;
+
+-- Feb 21, 2014 11:42:41 AM VET
+-- LVE Withholding
+UPDATE AD_Tab_Trl SET Name='Secuencia de Documento por Usuario',Updated=TO_DATE('2014-02-21 11:42:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53894 AND AD_Language='es_MX'
+;
+
+-- Feb 21, 2014 11:42:44 AM VET
+-- LVE Withholding
+UPDATE AD_Tab_Trl SET Name='Secuencia de Documento por Usuario',Updated=TO_DATE('2014-02-21 11:42:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53894 AND AD_Language='es_VE'
+;
+

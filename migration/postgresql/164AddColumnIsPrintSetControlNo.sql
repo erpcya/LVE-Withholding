51efe8413c5a827d31ce60cb4a9d94b71c514ba9
Dixon Martinez <dixon22ma@users.noreply.github.com>
2014-04-23 07:26:40 -0530

    Update migration scripts

--- migration/postgresql/164AddColumnIsPrintSetControlNo.sql
+++ migration/postgresql/164AddColumnIsPrintSetControlNo.sql
@@ -53,11 +53,6 @@ INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Refe
 INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=72597 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
 ;
 
--- Apr 3, 2014 4:35:34 PM VET
--- LVE Withholding
-ALTER TABLE LVE_WH_UserDocSequence ADD COLUMN IsPrintSetControlNo CHAR(1) DEFAULT 'N' CHECK (IsPrintSetControlNo IN ('Y','N'))
-;
-
 -- Apr 3, 2014 4:35:47 PM VET
 -- LVE Withholding
 INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,DisplayLength,EntityType,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,Updated,UpdatedBy) VALUES (0,72597,73268,0,53893,TO_TIMESTAMP('2014-04-03 16:35:45','YYYY-MM-DD HH24:MI:SS'),100,1,'ECA02','Y','Y','Y','N','N','N','N','N','Print Set Control No',TO_TIMESTAMP('2014-04-03 16:35:45','YYYY-MM-DD HH24:MI:SS'),100)

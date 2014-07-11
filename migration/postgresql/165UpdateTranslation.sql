ebdc14d96c9da82142bd2ca7eefa4aaf074ae02d
Dixon Martinez <dixon22ma@users.noreply.github.com>
2014-07-11 16:21:58 -0530

    Update Translation

--- migration/postgresql/165UpdateTranslation.sql
+++ migration/postgresql/165UpdateTranslation.sql
@@ -17,8 +17,3 @@ UPDATE AD_Menu_Trl SET Name='Reportes',Updated=TO_TIMESTAMP('2014-05-17 00:37:34
 -- LVE Withholding
 UPDATE AD_Menu_Trl SET Name='Reportes',Updated=TO_TIMESTAMP('2014-05-17 00:37:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Menu_ID=53800 AND AD_Language='es_VE'
 ;
-
-
-INSERT INTO AD_Menu_Trl VALUES(53587,'es_VE',0,0,'Y','2013-08-10 22:14:53',100,'2013-08-11 01:10:11',100,'Configuración','Configuración de Retenciones... Conceptos y Grupos','N');
-INSERT INTO AD_Menu_Trl VALUES(53588,'es_VE',0,0,'Y','2013-08-10 22:18:12',100,'2013-08-11 01:10:35',100,'Cálculo','Calculo de Retenciones y Declaración','N');
-INSERT INTO AD_Menu_Trl VALUES(53572,'es_VE',0,0,'Y','2013-07-30 12:05:41',100,'2013-07-30 12:05:49',100,'Retenciones','','N');
